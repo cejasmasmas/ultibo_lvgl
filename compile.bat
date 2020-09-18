8f54495f440eced16a7f9dfdd3b5849a92973898
cejasmasmas <65371443+cejasmasmas@users.noreply.github.com>
2020-09-18 11:29:37 -0600

    Add files via upload

--- compile.bat
+++ compile.bat
@@ -1,5 +1,5 @@
 del libultiboClvgl.a
-arm-none-eabi-gcc -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -D__DYNAMIC_REENT__ -c ultiboClvgl.c lvgl/src/lv_core/*.c lvgl/src/lv_draw/*.c lvgl/src/lv_font/*.c lvgl/src/lv_gpu/*.c lvgl/src/lv_hal/*.c lvgl/src/lv_misc/*.c lvgl/src/lv_themes/*.c lvgl/src/lv_widgets/*.c lv_examples/src/lv_demo_printer/*.c lv_examples/src/lv_demo_printer/images/*.c lv_examples/src/lv_demo_benchmark/*.c lv_examples/assets/*.c
+arm-none-eabi-gcc -DUltibo -D_POSIX_THREADS -O2 -mabi=aapcs -marm -march=armv7-a -mfpu=vfpv3-d16 -mfloat-abi=hard -D__DYNAMIC_REENT__ -c ultiboClvgl.c lvgl/src/lv_core/*.c lvgl/src/lv_draw/*.c lvgl/src/lv_font/*.c lvgl/src/lv_gpu/*.c lvgl/src/lv_hal/*.c lvgl/src/lv_misc/*.c lvgl/src/lv_themes/*.c lvgl/src/lv_widgets/*.c lv_examples/src/lv_demo_printer/*.c lv_examples/src/lv_demo_printer/images/*.c lv_examples/src/lv_demo_benchmark/*.c lv_examples/assets/*.c lv_examples/src/lv_demo_stress/*.c lv_examples/src/lv_demo_widgets/*.c lv_examples/src/lv_demo_keypad_encoder/*.c
 arm-none-eabi-ar rcs libultiboClvgl.a *.o
 @echo off
 del kernel7.img
